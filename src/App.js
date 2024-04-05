4fad524d31fd2a44f676e6cb92bb85b1f516d912
lavanya2729 <lavanyadeepak2911@gmail.com>
2024-04-05 17:59:32 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import logo from './logo.svg';
+
+import "./Assets/Css/Captial.css"
+import "./Assets/Css/Clarification.css"
+import Captial from './Components/Captial';
+import Clarification from './Components/Clarification'
 import './Assets/Css/Nav.css'
 import Contact from './Components/Contact';
 import Nav from './Components/Nav'
@@ -6,8 +10,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App"> 
+
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Nav />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+
     </div>
   )
 }
