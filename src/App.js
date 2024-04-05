1ca19b1a768608e685acef6d201c48775919b790
sangavi <sangavi.vijayaraghavan@axesstechnologymail.in>
2024-04-05 17:34:35 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Contact from './Components/Contact';
 
 function App() {
   return (
     <div className="App">
-      
+      <Contact/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './Assets/Css/contact.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap-icons/font/bootstrap-icons.css";
