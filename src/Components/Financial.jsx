694e785aaddfcf49494c8f5efc540ffff798af34
sangavi <sangavi.vijayaraghavan@axesstechnologymail.in>
2024-04-09 14:21:42 +0530

    responsive fixed

--- src/Components/Financial.jsx
+++ src/Components/Financial.jsx
@@ -65,26 +65,26 @@ function Financial() {
                         </div>
                         <div className='col-md-6 fin-section2'>
                             <div>
-                                <div className='section-head'>
-                                    <h4><span className='text-light heading4'>Plan for Your Tomorrow, </span> Today</h4>
-                                    <p className='text-light'>Building a secure financial future requires a well-rounded approach. At Sarvamangala Financial
+                                <div className=''>
+                                <h4 className='heading4 text-white'>Plan for Tommorrow, <span className='heading4'style={{color:"#900c3f"}}> Today</span></h4>
+                                    <p className='text-light heading3 text-white py-2'>Building a secure financial future requires a well-rounded approach. At Sarvamangala Financial
                                         Services, we offer comprehensive financial planning services that encompass:</p>
                                     {/* <p className='text-light'>At Sarvamangala Financial Services, we believe that a secure financial future is a foundation upon which you can build your dreams.</p> */}
                                 </div>
 
                                 <div className=''>
                                     <ul className='fin-ul'>
-                                        <li className=''> <span className='fs-5 fw-bold' style={{ color: '#f91942' }}>👉🏻 Investment Planning</span>
-                                            <p className='fin-li-p pt-2'>Grow your wealth and achieve your long-term goals.</p>
+                                        <li className=''> <span className='fs-5 heading3' style={{ color: '#900c3f' }}>👉🏻 Investment Planning</span>
+                                            <p className='fin-li-p heading3 text-dark'>Grow your wealth and achieve your long-term goals.</p>
                                         </li>
-                                        <li className=''> <span className='fs-5 fw-bold' style={{ color: '#f91942' }}>👉🏻 Retirement Planning</span>
-                                            <p className='fin-li-p pt-2'>Retire comfortably a personalized retirement plan.</p>
+                                        <li className=''> <span className='fs-5  heading3 ' style={{ color: '#900c3f' }}>👉🏻 Retirement Planning</span>
+                                            <p className='fin-li-p heading3 text-dark'>Retire comfortably a personalized retirement plan.</p>
                                         </li>
-                                        <li className=''> <span className='fs-5 fw-bold' style={{ color: '#f91942' }}>👉🏻 Tax Planning</span>
-                                            <p className='fin-li-p pt-2'> Minimize your tax burden and maximize your returns.</p>
+                                        <li className=''> <span className='fs-5 heading3' style={{ color: '#900c3f' }}>👉🏻 Tax Planning</span>
+                                            <p className='fin-li-p heading3 text-dark'> Minimize your tax burden & maximize your returns.</p>
                                         </li>
-                                        <li className=''> <span className='fs-5 fw-bold' style={{ color: '#f91942' }}>👉🏻 Estimate Planing</span>
-                                            <p className='fin-li-p pt-2'>Secure your loved ones' financial future.</p>
+                                        <li className=''> <span className='fs-5 heading3' style={{ color: '#900c3f' }}>👉🏻 Estimate Planing</span>
+                                            <p className='fin-li-p heading3 text-dark'>Secure your loved ones' financial future.</p>
                                         </li>
                                     </ul>
                                 </div>
