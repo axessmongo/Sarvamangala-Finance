64474404812b9783b20c185d738f527481aad2f3
nivethaaxess <nivetha.rajendran@axesstechnology.in>
2024-04-10 14:41:42 +0530

    added business page new

--- src/Components/Businessloan.jsx
+++ src/Components/Businessloan.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import "../Assets/Css/Businessloan.css"
-import carousel from "../Assets/img/carousel_img_2.svg"
-import carousel1 from "../Assets/img/carousel_img_3.svg"
-import carousel2 from "../Assets/img/carousel_img_4.svg"
+// import Nav from '../Components/Nav'
+// import carousel from "../Assets/img/carousel_img_2.svg"
+// import carousel1 from "../Assets/img/carousel_img_3.svg"
+// import carousel2 from "../Assets/img/carousel_img_4.svg"
+import sbi1 from "../Assets/img/sbi1.png"
 import Footer from './Footer'
 
 function Businessloan() {
@@ -14,13 +16,22 @@ function Businessloan() {
                 <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-inner container">
                         <div class="carousel-item active">
-                            <img src={carousel} alt="" />
+                            {/* <img src={carousel} alt="" /> */}
+                            <h1 className='business_banner_text'>Business Loan Upto</h1>
+                            {/* <p className='business_banner_text2 mb-3'>Loan Upto</p> */}
+                            <p className='business_banner_text2'>100 Crore</p>
                         </div>
                         <div class="carousel-item">
-                            <img src={carousel1} alt="" />
+                            {/* <img src={carousel1} alt="" /> */}
+                            <h1 className='business_banner_text'>Money In Your Bank In</h1>
+                            {/* <p className='business_banner_text2'>Your Bank In</p> */}
+                            <p className='business_banner_text2'>48 Hours</p>
                         </div>
                         <div class="carousel-item">
-                            <img src={carousel2} alt="" />
+                            {/* <img src={carousel2} alt="" /> */}
+                            <h1 className='business_banner_text'>Interest Rate As Low as</h1>
+                            {/* <p className='business_banner_text2'> As Low as</p> */}
+                            <p className='business_banner_text2'>1 Percent</p>
                         </div>
                     </div>
                 </div>
@@ -29,27 +40,106 @@ function Businessloan() {
             <section className='business mt-2'>
                 <div className='container'>
                     <div className='business_para'>
-                        <h2 className='text-center bus_texts heading4' style={{ color: "#900c3f" }} >WHAT IS A BUSINESS LOAN?</h2>
+                        <h2 className='text-center fs-1' style={{ color: "#900c3f" }} >WHAT IS A BUSINESS LOAN?</h2>
                         <p className='heading3'>A business loan is a financial product designed to provide companies with the capital they need for various purposes such as expansion, purchasing inventory or equipment, managing cash flow, or funding new projects. These loans are typically offered by banks, credit unions, or online lenders and can come in different forms such as term loans, lines of credit, equipment financing, or specialized loans like SBA loans. Businesses repay these loans over time with interest, helping them to access the funds necessary to grow and thrive.</p>
                         <p className='heading3'> Business loans play a crucial role in supporting the financial health and growth of companies across industries. They enable businesses to make strategic investments, seize opportunities, and navigate challenges by providing access to much-needed capital. By securing a business loan, companies can strengthen their operations, improve competitiveness, and achieve their long-term objectives.</p>
                     </div>
 
-                    <div className='container mt-5'>
-            <div className='business_eligi'>
-                <h3 className='bus_texts heading4 text-center'style={{ color: "#900c3f" }}>BUSINESS LOAN ELIGIBILITY CRITERIA</h3>
-                <div className="table-responsive mt-4  p-5">
-                    <table className="table heading3 ">
+
+                </div> <div className='background-container_1 mt-5'>
+                    <div className='container'>
+                        <h3 className='bus_texts heading4 text-center' style={{ color: "#900c3f" }}>BUSINESS LOAN ELIGIBILITY CRITERIA</h3>
+                        <div className="table-responsive mt-3">
+                            <table className="table heading3 " style={{border:"1px solid black"}}>
+                                <thead>
+                                    <tr>
+                                        <th className='table_text' scope="col" >Parameter</th>
+                                        <th className='table_text'>Description</th>
+                                    </tr>
+                                </thead>
+                                <tbody className='table_text2'>
+                                    <tr>
+                                        <th className='' scope="row">Mandatory Documents</th>
+                                        <td>
+                                            <ul className="list-unstyled ">
+                                                <li>Duly filled application form</li>
+                                                <li>PAN Card</li>
+                                            </ul>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th className='' scope="row">Proof of Identity</th>
+                                        <td>
+                                            <ul className="list-unstyled">
+                                                <li>Voter ID Card/ Photo ID issued by GOI/Passport/ Driving Licence/Aadhaar Card</li>
+                                            </ul>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th className='' scope="row">Proof of Address</th>
+                                        <td>
+                                            <ul className="list-unstyled">
+                                                <li>Aadhaar Card/ Voter ID Card/ Property Tax Receipt/ Driving Licence/ Utility Bill/ Registry Copy/Property Tax Receipt/ Lease or Rent Agreement</li>
+                                            </ul>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th className='' scope="row">Business Continuity Proof</th>
+                                        <td>
+                                            <ul className="list-unstyled">
+                                                <li>VAT/ Excise/ Sales Tax/ Service Tax Registration</li>
+                                                <li>Trade License/ Copy of partnership deed/ Registration Certificate issued by RBI or SEBI/ Certificate of Practice</li>
+                                            </ul>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th className='' scope="row">Signature Proof</th>
+                                        <td>
+                                            <ul className="list-unstyled">
+                                                <li>Banker's Verification/ Passport/ PAN Card</li>
+                                            </ul>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th className='' scope="row">Proof of Income</th>
+                                        <td>
+                                            <ul className="list-unstyled">
+                                                <li>Latest ITR</li>
+                                                <li>GST returns for the financial year</li>
+                                                <li>Latest audited financials</li>
+                                                <li>Last 6 months bank statement</li>
+                                            </ul>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th className='' scope="row">Other Documents (if applicable)</th>
+                                        <td>
+                                            <ul className="list-unstyled">
+                                                <li>List of Directors</li>
+                                                <li>Shareholding patterns</li>
+                                            </ul>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='business_eligi_2 mt-5 container     '>
+                    <h3 className='bus_texts heading4 text-center ' style={{ color: "#900c3f" }}>DOCUMENTS REQUIRED FOR A BUSINESS LOAN</h3>
+                    <table class="table mt-4 shadow heading3 " style={{border:"1px solid black"}}>
                         <thead>
                             <tr>
-                                <th scope="col" style={{color:"#900c3f"}}>Parameter</th>
-                                <th style={{color:"#900c3f"}}>Description</th>
+                                <th scope="col">Parameter</th>
+                                <td>Description</td>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
                                 <th className='' scope="row">Mandatory Documents</th>
                                 <td>
-                                    <ul className="list-unstyled">
+                                    <ul style={{ listStyleType: "square" }}>
                                         <li>Duly filled application form</li>
                                         <li>PAN Card</li>
                                     </ul>
@@ -58,7 +148,7 @@ function Businessloan() {
                             <tr>
                                 <th className='' scope="row">Proof of Identity</th>
                                 <td>
-                                    <ul className="list-unstyled">
+                                    <ul style={{ listStyleType: "square" }}>
                                         <li>Voter ID Card/ Photo ID issued by GOI/Passport/ Driving Licence/Aadhaar Card</li>
                                     </ul>
                                 </td>
@@ -66,7 +156,7 @@ function Businessloan() {
                             <tr>
                                 <th className='' scope="row">Proof of Address</th>
                                 <td>
-                                    <ul className="list-unstyled">
+                                    <ul style={{ listStyleType: "square" }}>
                                         <li>Aadhaar Card/ Voter ID Card/ Property Tax Receipt/ Driving Licence/ Utility Bill/ Registry Copy/Property Tax Receipt/ Lease or Rent Agreement</li>
                                     </ul>
                                 </td>
@@ -74,7 +164,7 @@ function Businessloan() {
                             <tr>
                                 <th className='' scope="row">Business Continuity Proof</th>
                                 <td>
-                                    <ul className="list-unstyled">
+                                    <ul style={{ listStyleType: "square" }}>
                                         <li>VAT/ Excise/ Sales Tax/ Service Tax Registration</li>
                                         <li>Trade License/ Copy of partnership deed/ Registration Certificate issued by RBI or SEBI/ Certificate of Practice</li>
                                     </ul>
@@ -83,7 +173,7 @@ function Businessloan() {
                             <tr>
                                 <th className='' scope="row">Signature Proof</th>
                                 <td>
-                                    <ul className="list-unstyled">
+                                    <ul style={{ listStyleType: "square" }}>
                                         <li>Banker's Verification/ Passport/ PAN Card</li>
                                     </ul>
                                 </td>
@@ -91,7 +181,7 @@ function Businessloan() {
                             <tr>
                                 <th className='' scope="row">Proof of Income</th>
                                 <td>
-                                    <ul className="list-unstyled">
+                                    <ul style={{ listStyleType: "square" }}>
                                         <li>Latest ITR</li>
                                         <li>GST returns for the financial year</li>
                                         <li>Latest audited financials</li>
@@ -102,7 +192,7 @@ function Businessloan() {
                             <tr>
                                 <th className='' scope="row">Other Documents (if applicable)</th>
                                 <td>
-                                    <ul className="list-unstyled">
+                                    <ul style={{ listStyleType: "square" }}>
                                         <li>List of Directors</li>
                                         <li>Shareholding patterns</li>
                                     </ul>
@@ -110,85 +200,6 @@ function Businessloan() {
                             </tr>
                         </tbody>
                     </table>
-                </div>
-            </div>
-        </div>
-
-                    <div className='business_eligi_2 mt-5'>
-                        <h3 className='bus_texts heading4 text-center' style={{ color: "#900c3f" }}>DOCUMENTS REQUIRED FOR A BUSINESS LOAN</h3>
-                        <table class="table mt-4 shadow heading3 ">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Parameter</th>
-                                    <td>Description</td>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th className='' scope="row">Mandatory Documents</th>
-                                    <td>
-                                        <ul style={{ listStyleType: "square" }}>
-                                            <li>Duly filled application form</li>
-                                            <li>PAN Card</li>
-                                        </ul>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th className='' scope="row">Proof of Identity</th>
-                                    <td>
-                                        <ul style={{ listStyleType: "square" }}>
-                                            <li>Voter ID Card/ Photo ID issued by GOI/Passport/ Driving Licence/Aadhaar Card</li>
-                                        </ul>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th className='' scope="row">Proof of Address</th>
-                                    <td>
-                                        <ul style={{ listStyleType: "square" }}>
-                                            <li>Aadhaar Card/ Voter ID Card/ Property Tax Receipt/ Driving Licence/ Utility Bill/ Registry Copy/Property Tax Receipt/ Lease or Rent Agreement</li>
-                                        </ul>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th className='' scope="row">Business Continuity Proof</th>
-                                    <td>
-                                        <ul style={{ listStyleType: "square" }}>
-                                            <li>VAT/ Excise/ Sales Tax/ Service Tax Registration</li>
-                                            <li>Trade License/ Copy of partnership deed/ Registration Certificate issued by RBI or SEBI/ Certificate of Practice</li>
-                                        </ul>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th className='' scope="row">Signature Proof</th>
-                                    <td>
-                                        <ul style={{ listStyleType: "square" }}>
-                                            <li>Banker's Verification/ Passport/ PAN Card</li>
-                                        </ul>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th className='' scope="row">Proof of Income</th>
-                                    <td>
-                                        <ul style={{ listStyleType: "square" }}>
-                                            <li>Latest ITR</li>
-                                            <li>GST returns for the financial year</li>
-                                            <li>Latest audited financials</li>
-                                            <li>Last 6 months bank statement</li>
-                                        </ul>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th className='' scope="row">Other Documents (if applicable)</th>
-                                    <td>
-                                        <ul style={{ listStyleType: "square" }}>
-                                            <li>List of Directors</li>
-                                            <li>Shareholding patterns</li>
-                                        </ul>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
                 </div>
             </section>
             <Footer />
