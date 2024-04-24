c0030531c90baeb4d9b832dfea7ae0f54e88c1d5
Jagan <jagannathan880@gmail.com>
2024-04-24 14:02:07 +0530

    new changes blogs

--- frontend/src/Blogs/Blog2.jsx
+++ frontend/src/Blogs/Blog2.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 import logo from "../Assets/img/logo1.png";
 import Footer from "../Components/Footer";
 import Scroll from '../Components/Scroll'
+import axios from 'axios'
 import {
   slice, concat,
 } from 'lodash';
@@ -26,6 +27,20 @@ function Blog2() {
     setList(newList);
     setShowMore(newShowMore);
   }
+  const [socialLinks, setSocialLinks] = useState([]);
+
+  useEffect(() => {
+      const fetchSocialLinks = async () => {
+          try {
+              const response = await axios.get("api/get");
+              setSocialLinks(response.data.data[0].count);
+          } catch (error) {
+              console.error("Error fetching social links:", error);
+          }
+      };
+
+      fetchSocialLinks();
+  }, []);
 
 
   return (
@@ -63,34 +78,39 @@ function Blog2() {
                         class="nav-link active text-white"
                         aria-current="page"
                         id="name-nav"
-                        href="home"
+                        href="/"
                       >
                         Home
                       </a>
                     </li>
 
                     <li class="nav-item">
-                      <Link className="nav-link" to="goals">
+                    <a class="nav-link" href="/" id="goals">
                         Goals
-                      </Link>
+                        </a>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link" href="#" id="chooseus">
+                      <a class="nav-link" href="/" id="chooseus">
                         ChooseUs
                       </a>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link" href="#" id="services">
+                      <a class="nav-link" href="/" id="services">
                         Services
                       </a>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link" href="#" id="plans">
+                      <a class="nav-link" href="/" id="plans">
                         Plans
                       </a>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link" href="#" id="contact">
+                      <a class="nav-link" href="/blogs">
+                        Blogs
+                      </a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="/" id="contact">
                         Contact
                       </a>
                     </li>
@@ -102,19 +122,7 @@ function Blog2() {
         </nav>
       </section>
       <section className=" overflow-x-hidden mt-5 mt-lg-0" data-aos="fade-down">
-        {/* <div className="fall fall1">
-          <img src={coins} alt={coins} />
-        </div>
-        <div className="fall fall2">
-          <img src={coins} alt={coins} />
-        </div> */}
         <div className="hero" id="home1">
-          {/* <div className=" py-5 heading3 btn1">
-            <button>
-              <Link to={"/"}>Back To Home</Link>
-            </button>
-          </div> */}
-          {/* <div className="hero-opacity text-white"></div> */}
         </div>
       </section>
       <div>
@@ -163,7 +171,97 @@ function Blog2() {
         </div>
 
       </section>
-      <Footer />
+      <footer className="footer-section mt-4">
+            <div className="container">
+                <div className="footer-cta pt-5 ">
+                    <div className="row">
+                        <div className="col-xl-4 col-md-4 mb-30"></div>
+                        <div className="col-xl-4 col-md-4 mb-30">
+                            <div className="single-cta">
+                            </div>
+                        </div>
+                        <div className="col-xl-4 col-md-4 mb-30">
+                        </div>
+                    </div>
+                </div>
+                <div className="footer-content pt-5">
+                    <div className="row">
+                        <div className="col-xl-4 col-lg-4 ">
+                            <div className="footer-widget">
+                                <div className="footer-logo nature-1">
+                                    <img src={logo} className="img-fluid" alt="logo" />
+                                </div>
+                                {/* <div className="footer-text">
+                                    <p className='heading3'>Lorem ipsum dolor sit amet, consec tetur adipisicing elit, sed do eiusmod tempor incididuntut consec tetur adipisicing
+                                        elit,Lorem ipsum dolor sit amet.</p>
+                                </div> */}
+                                <div className="footer-social-icon nature-1">
+                                    <span className='heading5 text-white'>Follow us </span>
+                                    <div class="social-icons">
+
+                                        <div class="social-buttons">
+                                            {/* <a href="https://web.whatsapp.com/" target="blank" class="social-margin">
+                                                <div class="social-icon whatsapp">
+                                                    <i class="bi bi-whatsapp" aria-hidden="true"></i>
+                                                </div>
+                                            </a> */}
+                                            <a href="https://www.facebook.com/profile.php?id=61553902680645" target="blank" class="social-margin">
+                                                <div class="social-icon facebook">
+                                                    <i class="bi bi-facebook"></i>
+                                                </div>
+                                            </a>
+                                            <a href="https://www.instagram.com/sarvamangalafinancialservices/" target="blank" class="social-margin">
+                                                <div class="social-icon instagram">
+                                                    <i class="bi bi-instagram"></i>
+                                                </div>
+                                            </a>
+
+                                            {/* <a href="http://twitter.com/" target="blank" class="social-margin">
+                                                <div class="social-icon twitter">
+                                                    <i class="bi bi-twitter"></i>
+                                                </div>
+                                            </a> */}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-xl-4 col-lg-4 col-md-6 ">
+                            <div className="footer-widget">
+                                <div className="footer-widget-heading">
+                                    <h3 className='heading3'>Useful Links</h3>
+                                </div>
+                                <ul className='heading3'>
+                                    <li><a href="/" >Home</a></li>
+                                    <li><a href="/">Goals</a></li>
+                                    <li><a href="/">services</a></li>
+                                    <li><a href="/">Contact</a></li>
+                                    <li><a href="/">Plans</a></li>
+
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-xl-4 col-lg-4 col-md-6">
+                            <div className="footer-widget">
+                                <div className="footer-widget-heading">
+                                    <h3 className='heading5'>Subscribe</h3>
+                                </div>
+                                <div className="footer-text">
+                                    <p className='heading3'>Get exclusive insights & offers. Follow us on Instagram & Facebook! {socialLinks}
+                                    </p>
+                                </div>
+                                {/* <div className='heading3 btn2 text-center mt-3'>
+                                    <button className='' >
+                                        <Link to={"contact"}>Contact Us</Link>
+                                    </button>
+                                </div> */}
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
       <Scroll />
     </div>
   );
